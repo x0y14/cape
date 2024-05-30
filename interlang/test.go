eb3f460f052d9b578649597b74f4ce84e39ad834
x0y14 <ya01ma08@gmail.com>
2024-05-30 18:00:50 +0900

    コメントアウト

---
+++ interlang/test.go
@@ -0,0 +1,10 @@
+package interlang
+
+//func gen() {
+//	node := NewNode(VariableDeclare, &VariableDeclareField{Ident: "a"})
+//	switch node.GetKind() {
+//	case VariableDeclare:
+//		_ = node.GetField().(*VariableDeclareField)
+//	default:
+//	}
+//}
